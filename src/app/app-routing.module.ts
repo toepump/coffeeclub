e560ed56f527e29bb51bf382c9c2e7d5b21b23cd
Mikey Kentaro <mikeykentarocho@gmail.com>
2020-04-09 13:33:00 -0700

    Accidently removed home routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', redirectTo: 'home', pathMatch: 'full'},
   {
     path: 'coffee-list',
     loadChildren: () => import('./coffee-list/coffee-list.module').then(m => m.CoffeeListModule),
